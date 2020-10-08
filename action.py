debc3442ca41f9ab1d05fa7d09046c2408640b00
Carles Cufi <carles.cufi@nordicsemi.no>
2020-10-08 19:51:00 +0200

    Add an action.py
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

---
+++ action.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import sys
+
+def main():
+    #token = os.environ.get('GH_TOKEN', None)
+    #if not token:
+    #    sys.exit('Github token not set in environment, please set the GH_TOKEN'
+    #             ' environment variable and retry.')
+
+    #gh = Github(token)
+    #args = parse_args(sys.argv[1:], gh)
+
+    print('Output from Python')
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
