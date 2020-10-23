fb7d0840e82200399f4a152a23a5181087888db0
Carles Cufi <carles.cufi@nordicsemi.no>
2020-10-23 14:49:05 +0200

    Add a github token input
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- action.py
+++ action.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 
 def main():
-    #token = os.environ.get('GH_TOKEN', None)
-    #if not token:
-    #    sys.exit('Github token not set in environment, please set the GH_TOKEN'
-    #             ' environment variable and retry.')
+    token = os.environ.get('GITHUB_TOKEN', None)
+    if not token:
+        sys.exit('Github token not set in environment, please set the'
+                 'GITHUB_TOKEN environment variable and retry.')
 
     #gh = Github(token)
     #args = parse_args(sys.argv[1:], gh)
 
     print('Output from Python')
+    print(f'token: {token}')
     sys.exit(0)
-
 
 if __name__ == '__main__':
     main()
