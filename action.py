6ac9974041363c9600e638d0f1af169e00eda88d
Carles Cufi <carles.cufi@nordicsemi.no>
2020-10-23 17:52:29 +0200

    Interface with PyGithub
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- action.py
+++ action.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 
+from github import Github, GithubException
 import os
 import sys
 
 def main():
     token = os.environ.get('GITHUB_TOKEN', None)
     if not token:
-        sys.exit('Github token not set in environment, please set the'
+        sys.exit('Github token not set in environment, please set the '
                  'GITHUB_TOKEN environment variable and retry.')
 
-    #gh = Github(token)
+    gh = Github(token)
+    org = gh.get_organization('nrfconnect')
+    team = org.get_team_by_slug('ncs-code-owners')
+    for m in team.get_members():
+        print(m.login)
+
     #args = parse_args(sys.argv[1:], gh)
 
     print('Output from Python')
