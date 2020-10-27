6d9531c860fd4da7c7672f72f4f00d2a3c3814f6
Carles Cufi <carles.cufi@nordicsemi.no>
2020-10-27 11:26:58 +0100

    Add support for input parameters
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- action.py
+++ action.py
@@ -1,10 +1,39 @@
 #!/usr/bin/env python3
 
+import argparse
 from github import Github, GithubException
 import os
 import sys
 
+def gh_tuple_split(s):
+    sl = s.split('/')
+    if len(sl) != 2:
+        raise RuntimeError("Invalid org or dst format")
+
+    return sl[0], sl[1]
+
 def main():
+
+    parser = argparse.ArgumentParser(
+        description="GH Action script for contribution management",
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('-c', '--command', action='store',
+                        choices=['external'],
+                        required=True,
+                        help='Command to execute.')
+
+    parser.add_argument('-m', '--message', action='store',
+                        required=False,
+                        help='Message to post.')
+
+    parser.add_argument('-l', '--labels', action='store',
+                        required=False,
+                        help='Comma-separated list of labels.')
+
+    print(sys.argv)
+
+    args = parser.parse_args()
+
 
     # Retrieve main env vars
     action = os.environ.get('GITHUB_ACTION', None)
@@ -25,21 +54,20 @@ def main():
                  'GITHUB_TOKEN environment variable and retry.')
 
     gh = Github(token)
-    org = gh.get_organization('nrfconnect')
+
+    org, repo = gh_tuple_split(repo)
+
+    org = gh.get_organization(org)
     #team = org.get_team_by_slug('ncs-code-owners')
     #for m in team.get_members():
     #    print(m.login)
-    repo = gh.get_repo('nrfconnect/sdk-nrf')
-    i = 0
-    for p in repo.get_pulls():
-        print(f'{p.number}: {p.title}')
-        i = i+1
-        if i > 10:
-            break
-
-    print(sys.argv)
-    #args = parse_args(sys.argv[1:], gh)
-
+    #repo = gh.get_repo('nrfconnect/sdk-nrf')
+    #i = 0
+    #for p in repo.get_pulls():
+    #    print(f'{p.number}: {p.title}')
+    #    i = i+1
+    #    if i > 10:
+    #        break
     sys.exit(0)
 
 if __name__ == '__main__':
